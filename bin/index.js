e55397cae6e30a9854a2e23294727c6f7e505c01
Cardenaz <william@cardenas.se>
2021-07-03 16:09:00 +0200

    sec

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,14 @@
 #! /usr/bin/env node
+
+const yargs = require("yargs");
+
 console.log("Lulo CLI"); 
+
+
+const usage = "\nUsage: tran <lang_name> sentence to be translated";
+const options = yargs  
+      .usage(usage)  
+      .option("l", {alias:"collection", describe: "List all collections.", type: "boolean", demandOption
+: false })                                                                                                    
+      .help(true)  
+      .argv;
